80f4617416233b56f89481da7a275953988ec2a1
alexander <alexkaranja629@gmail.com>
2021-08-27 19:25:58 -0700

    added bookings Crud ops

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -6,8 +6,81 @@ let bookingsObjects = JSON.parse(bookings);
 exports.getAllBooking = function (req, res, next) {
   res.json({
     status: "sucess",
+    response: bookingsObjects.length,
     data: {
       bookingsObjects,
     },
+  });
+};
+
+exports.add = function (req, res) {
+  let data = req.body;
+  let id = { id: Math.random() };
+  let newObject = Object.assign(id, data);
+  bookingsObjects.push(newObject);
+
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/bookings.json`,
+    JSON.stringify(bookingsObjects),
+    function (error) {
+      if (error) {
+        res.json({
+          status: "failed",
+          error,
+        });
+      } else {
+        res.status(201).json({
+          status: "success",
+          data: {
+            newObject,
+          },
+        });
+      }
+    }
+  );
+};
+
+exports.getOne = function name(req, res, next) {
+  let parameter = req.params;
+  let data = bookingsObjects.find(function (element) {
+    return element.id == parameter.id;
+  });
+  res.json({
+    status: "succeszs",
+    data,
+  });
+};
+
+exports.delete = function (req, res, next) {
+  let id = req.params.id;
+
+  let data = bookingsObjects.filter(function (element) {
+    return element.id != id;
+  });
+
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/bookings.json`,
+    JSON.stringify(data),
+    function (err) {
+      if (err) {
+        res.json({
+          status: "failed",
+          error,
+        });
+      } else {
+        res.status(204).json({
+          status: "success",
+        });
+      }
+    }
+  );
+};
+
+exports.update = function (req, res, next) {
+  let id = req.params.id;
+
+  res.json({
+    status: "success",
+    details: "Not yet implemented",
   });
 };
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,5 +4,9 @@ let express = require("express");
 let routes = express.Router();
 
 routes.get("/", bookingController.getAllBooking);
+routes.post("/", bookingController.add);
+routes.get("/:id", bookingController.getOne);
+routes.delete("/:id", bookingController.delete);
+routes.patch("/:id", bookingController.update);
 
 module.exports = routes;
