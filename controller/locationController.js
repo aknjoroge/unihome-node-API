435eaadd020b7efa13e91f382535ca98cea5c5e9
alexander <alexkaranja629@gmail.com>
2021-08-27 19:33:33 -0700

    added crud to location module

--- controller/locationController.js
+++ controller/locationController.js
@@ -6,8 +6,81 @@ let locationObjects = JSON.parse(location);
 exports.getAllLocation = function (req, res, next) {
   res.json({
     status: "sucess",
+    response: locationObjects.length,
     data: {
       locationObjects,
     },
+  });
+};
+
+exports.add = function (req, res) {
+  let data = req.body;
+  let id = { id: Math.random() };
+  let newObject = Object.assign(id, data);
+  locationObjects.push(newObject);
+
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/location.json`,
+    JSON.stringify(locationObjects),
+    function (error) {
+      if (error) {
+        res.json({
+          status: "failed",
+          error,
+        });
+      } else {
+        res.status(201).json({
+          status: "success",
+          data: {
+            newObject,
+          },
+        });
+      }
+    }
+  );
+};
+
+exports.getOne = function name(req, res, next) {
+  let parameter = req.params;
+  let data = locationObjects.find(function (element) {
+    return element.id == parameter.id;
+  });
+  res.json({
+    status: "success",
+    data,
+  });
+};
+
+exports.delete = function (req, res, next) {
+  let id = req.params.id;
+
+  let data = locationObjects.filter(function (element) {
+    return element.id != id;
+  });
+
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/location.json`,
+    JSON.stringify(data),
+    function (err) {
+      if (err) {
+        res.json({
+          status: "failed",
+          error,
+        });
+      } else {
+        res.status(204).json({
+          status: "success",
+        });
+      }
+    }
+  );
+};
+
+exports.update = function (req, res, next) {
+  let id = req.params.id;
+
+  res.json({
+    status: "success",
+    details: "Not yet implemented",
   });
 };
--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -4,5 +4,9 @@ let express = require("express");
 let routes = express.Router();
 
 routes.get("/", locationController.getAllLocation);
+routes.post("/", locationController.add);
+routes.get("/:id", locationController.getOne);
+routes.delete("/:id", locationController.delete);
+routes.patch("/:id", locationController.update);
 
 module.exports = routes;
