ede6d7e260ca25ba6714f9f36a5119855fd4b4e0
alexander <alexkaranja629@gmail.com>
2021-08-27 18:50:49 -0700

    implemented CRUD to owners module

--- controller/ownersController.js
+++ controller/ownersController.js
@@ -8,8 +8,83 @@ let ownersData = JSON.parse(owners);
 exports.getOwners = function (req, res, next) {
   res.json({
     status: "success",
+    response: ownersData.length,
     data: {
       ownersData,
     },
+  });
+};
+
+exports.addOwner = function (req, res) {
+  let owner = req.body;
+  let id = { id: Math.random() };
+  let newOwner = Object.assign(id, owner);
+  ownersData.push(newOwner);
+
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/owners.json`,
+    JSON.stringify(ownersData),
+    function (error) {
+      if (error) {
+        res.json({
+          status: "failed",
+          error,
+        });
+      } else {
+        res.status(201).json({
+          status: "success",
+          data: {
+            newOwner,
+          },
+        });
+      }
+    }
+  );
+};
+
+exports.getOneOwner = function name(req, res, next) {
+  let parameter = req.params;
+  let owner = ownersData.find(function (element) {
+    return element.id == parameter.id;
+  });
+  res.json({
+    status: "succeszs",
+    data: {
+      owner,
+    },
+  });
+};
+
+exports.deletOwner = function (req, res, next) {
+  let id = req.params.id;
+
+  let owners = ownersData.filter(function (element) {
+    return element.id != id;
+  });
+
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/owners.json`,
+    JSON.stringify(owners),
+    function (err) {
+      if (err) {
+        res.json({
+          status: "failed",
+          error,
+        });
+      } else {
+        res.status(204).json({
+          status: "success",
+        });
+      }
+    }
+  );
+};
+
+exports.updateOwner = function (req, res, next) {
+  let id = req.params.id;
+
+  res.json({
+    status: "success",
+    details: "Not yet implemented",
   });
 };
--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -4,5 +4,9 @@ let ownersController = require("./../controller/ownersController");
 let routes = express.Router();
 
 routes.get("/", ownersController.getOwners);
+routes.post("/", ownersController.addOwner);
+routes.get("/:id", ownersController.getOneOwner);
+routes.delete("/:id", ownersController.deletOwner);
+routes.patch("/:id", ownersController.updateOwner);
 
 module.exports = routes;
