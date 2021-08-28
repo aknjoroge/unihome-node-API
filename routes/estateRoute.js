faddd1e71a4f88741ea8eb442a00774990803508
alexander <alexkaranja629@gmail.com>
2021-08-27 19:15:39 -0700

    added estate  module crud operations

--- routes/estateRoute.js
+++ routes/estateRoute.js
@@ -4,5 +4,9 @@ let express = require("express");
 let routes = express.Router();
 
 routes.get("/", estateController.getAllEstates);
+routes.get("/:id", estateController.getOne);
+routes.post("/", estateController.add);
+routes.delete("/:id", estateController.delete);
+routes.patch("/", estateController.update);
 
 module.exports = routes;
